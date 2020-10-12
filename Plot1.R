d649c4c2ffb5f6afff019fb78d7fcc561ae3e455
biswajit2k4 <biswajit2k4@gmail.com>
2020-10-12 14:02:51 +0530

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,8 @@
+# set working directory
+setwd("~/GitHub/EDA_Project1")
+
+source("LoadFilteredData.R")
+png(filename = "Plot1.png", width = 480, height = 480, units = "px")
+hist(Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", breaks = 12, ylim = c(0, 1200))
+dev.off()
+rm(data)
---
+++ Plot2.R
@@ -0,0 +1,10 @@
+# set working directory
+setwd("~/GitHub/EDA_Project1")
+
+source("LoadFilteredData.R")
+png(filename = "Plot2.png", width = 480, height = 480, units = "px")
+plot(Date_time, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)", xaxt = "n")
+axis(1, at = c(1, 1441, 2880), labels = c("Thu", "Fri", "Sat"))
+lines(Date_time, Global_active_power, col = "black")
+dev.off()
+rm(data)
